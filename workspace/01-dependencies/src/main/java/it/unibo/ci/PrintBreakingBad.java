d70e5397488d6ba4172a08f81f89b975f9b6f181
Danilo Pianini <danilo.pianini@unibo.it>
2018-04-16 18:54:34 +0200

    Prefer importing files over copying

--- workspace/01-dependencies/src/main/java/it/unibo/ci/PrintBreakingBad.java
+++ workspace/01-dependencies/src/main/java/it/unibo/ci/PrintBreakingBad.java
@@ -1,25 +1,17 @@
 package it.unibo.ci;
-
 import java.io.IOException;
-
 import org.apache.commons.io.IOUtils;
-
 import com.google.common.base.Charsets;
 import com.omertron.thetvdbapi.TheTVDBApi;
 import com.omertron.thetvdbapi.model.Episode;
 import com.omertron.thetvdbapi.model.Series;
 
 public final class PrintBreakingBad {
-
     private static final String LANG = "it";
     private static final String SERIE = "Breaking Bad";
-
-    private PrintBreakingBad() {
-    }
-
+    private PrintBreakingBad() { }
     public static void main(String... args) throws ClassNotFoundException, IOException {
-        final String key = IOUtils.toString(PrintBreakingBad.class.getResourceAsStream("/TheTVDBAPIKey"),
-                Charsets.UTF_8);
+        final String key = IOUtils.toString(PrintBreakingBad.class.getResourceAsStream("/TheTVDBAPIKey"), Charsets.UTF_8);
         final TheTVDBApi api = new TheTVDBApi(key);
         api.searchSeries("Breaking Bad", LANG).stream()
             .filter(s -> s.getSeriesName().equals(SERIE))
